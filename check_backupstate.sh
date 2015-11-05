fd8a339fb425e6a2f60f89530a2b324346a415db
Ben Matheja <post@benmatheja.de>
2015-11-05 11:01:00 +0100

    debug echo's commented out

--- check_backupstate.sh
+++ check_backupstate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Configuration
 timespan=1814400
-threshold_warn=0.75
+threshold_warn=0.70
 threshold_crit=0.25
 now=$(date +%s)
 #cd /home/nagios
@@ -21,7 +21,7 @@ echo "0" >> total_not_having_counter
 echo "0" >> total_backup_succesful_quota
 
 #Check TimeMachine in /srv/backup
-echo -e "\n$(date +'%a %d %b - %T') Check Timemachine Backup Success"
+echo -e "\n$(date +'%a %d %b - %T') Check Timemachine Backup Success" >> check_backupstate.log
 
 cat /etc/passwd | cut -f1 -d : > passwd_out
 grep -Fxv -f blacklist passwd_out | while read LINE
@@ -33,7 +33,7 @@ do
 	if [ $( ls -l /srv/backup | grep -o $LINE | wc -l) -gt 0 ]; then
 	#Are there multiple dates? If yes, skip to next iteration of while loop
 		if [ $(ls --full-time /srv/backup | grep $LINE | awk '{print $6}' | wc -l) -gt 1 ]; then
-			echo "$(date +'%a %d %b - %T') $LINE is having multiple backup folders"
+			echo "$(date +'%a %d %b - %T') $LINE is having multiple backup folders" >> check_backupstate.log
 			continue
 	#Calculate epoch from last_backup
 		else
@@ -43,16 +43,16 @@ do
 		delta=`expr $now - $last_backup`
 	#Transform 3 weeks in seconds and compare against delta 3weeks=1814400
 		if [ $delta -gt $timespan ]; then
-			echo "$(date +'%a %d %b - %T') $LINE is having old backup"
+			echo "$(date +'%a %d %b - %T') $LINE is having old backup" >> check_backupstate.log
 			old_backup_counter=$((old_backup_counter + 1))
 			echo $old_backup_counter > tm_old_backup_counter
 		else
-			echo "$(date +'%a %d %b - %T') $LINE is having active Backup"
+			echo "$(date +'%a %d %b - %T') $LINE is having active Backup" >> check_backupstate.log
 			active_backup_counter=$((active_backup_counter + 1))
 			echo $active_backup_counter > tm_active_backup_counter
 		fi
 	else
-		echo "$(date +'%a %d %b - %T') $LINE is not having a Backup"
+		echo "$(date +'%a %d %b - %T') $LINE is not having a Backup" >> check_backupstate.log
 		not_having_counter=$((not_having_counter + 1))
 		echo $not_having_counter > tm_not_having_counter
 	fi
@@ -65,7 +65,7 @@ tm_old_backup_counter=$(cat tm_old_backup_counter)
 tm_not_having_counter=$(cat tm_not_having_counter)
 
 #----------------------------------------------------------------------------------------------------------
-echo -e "\n$(date +'%a %d %b - %T') Check SFTP Backup Success"
+echo -e "\n$(date +'%a %d %b - %T') Check SFTP Backup Success" >> check_backupstate.log
 grep -Fx -f blacklist linux_users | while read LINE
 do
 	#echo "$(date +'%a %d %b - %T') Checking Backup Status for $LINE " >> check_tmstate.log
@@ -75,7 +75,7 @@ do
 	if [ $( ls -l /srv/sftp | grep -o $LINE | wc -l) -gt 0 ]; then
 	#Are there multiple dates? If yes, skip to next iteration of while loop
 		if [ $(ls --full-time /srv/sftp | grep $LINE | awk '{print $6}' | wc -l) -gt 1 ]; then
-			echo "$(date +'%a %d %b - %T') $LINE is having multiple backup folders"
+			echo "$(date +'%a %d %b - %T') $LINE is having multiple backup folders" >> check_backupstate.log
 			continue
 	#Calculate epoch from last_backup
 		else
@@ -85,16 +85,16 @@ do
 		delta=`expr $now - $last_backup`
 	#Transform 3 weeks in seconds and compare against delta 3weeks=1814400
 		if [ $delta -gt $timespan ]; then
-			echo "$(date +'%a %d %b - %T') $LINE is having old backup" 
+			echo "$(date +'%a %d %b - %T') $LINE is having old backup" >> check_backupstate.log
 			old_backup_counter=$((old_backup_counter + 1))
 			echo $old_backup_counter > sf_old_backup_counter
 		else
-			echo "$(date +'%a %d %b - %T') $LINE is having active Backup" 
+			echo "$(date +'%a %d %b - %T') $LINE is having active Backup" >> check_backupstate.log
 			active_backup_counter=$((active_backup_counter + 1))
 			echo $active_backup_counter > sf_active_backup_counter
 		fi
 	else
-		echo "$(date +'%a %d %b - %T') $LINE is not having a Backup"
+		echo "$(date +'%a %d %b - %T') $LINE is not having a Backup" >> check_backupstate.log
 		not_having_counter=$((not_having_counter + 1))
 		echo $not_having_counter > sf_not_having_counter
 	fi
@@ -105,20 +105,19 @@ sf_old_backup_counter=$(cat sf_old_backup_counter)
 sf_not_having_counter=$(cat sf_not_having_counter)
 
 total_users_checked=`expr $tm_users_checked + $sf_users_checked`
-echo "total users checked: $total_users_checked = $tm_users_checked + $sf_users_checked"
+#echo "total users checked: $total_users_checked = $tm_users_checked + $sf_users_checked" 
 
 total_active_backup_counter=`expr $tm_active_backup_counter + $sf_active_backup_counter`
-echo "total active backup counter: $total_active_backup_counter = $tm_active_backup_counter + $sf_active_backup_counter"
+#echo "total active backup counter: $total_active_backup_counter = $tm_active_backup_counter + $sf_active_backup_counter"
 #! bangs because sf_active_backup_counter is null but not zero
 
 total_old_backup_counter=`expr $tm_old_backup_counter + $sf_old_backup_counter`
-echo "total old backup counter: $total_old_backup_counter"
+#echo "total old backup counter: $total_old_backup_counter"
 total_not_having_counter=`expr $tm_not_having_counter + $sf_not_having_counter`
-echo "total not having counter : $total_not_having_counter"
+#echo "total not having counter : $total_not_having_counter"
 total_backup_succesful_quota=$(bc <<< "scale=1;$total_active_backup_counter/$total_users_checked" | awk '{printf "%f", $0}')
-echo "total backup Successful quota: $total_backup_succesful_quota"
+#echo "total backup Successful quota: $total_backup_succesful_quota"
 
-echo "Write into Logfile"
 infotxt="\nTotal Users Checked: $total_users_checked \nActive Backups found: $total_active_backup_counter \nOutdated Backups found: $total_old_backup_counter \nUsers not having backups: $total_not_having_counter \nBackups Successful Quota: $total_backup_succesful_quota\nFor more information see /home/nagios/check_backupstate.log"
 echo -e "$infotxt" >> check_backupstate.log
 
@@ -136,6 +135,6 @@ fi
 echo -e "Backup is $statustxt $infotxt"
 
 #Cleanup
-rm tm_users_checked tm_active_backup_counter tm_old_backup_counter tm_not_having_counter sf_users_checked sf_active_backup_counter sf_old_backup_counter sf_not_having_counter passwd_out total_backup_succesful_quota
+rm tm_users_checked tm_active_backup_counter tm_old_backup_counter tm_not_having_counter sf_users_checked sf_active_backup_counter sf_old_backup_counter sf_not_having_counter passwd_out total_backup_succesful_quota 	total_active_backup_counter	total_not_having_counter total_old_backup_counter total_users_checked
 exit $status
 
